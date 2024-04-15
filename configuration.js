49e08a2be8e7c51b3792109eb27d3a3374766458
connortroyrogers <connortroyrogers@gmail.com>
2024-04-14 21:13:20 -0400

    update for homework 8

--- configuration.js
+++ configuration.js
@@ -2,7 +2,7 @@ const mode = 0;
 
 const host_local = "http://localhost:8080";
 
-const host_remote = "http://localhost:8080";
+const host_remote = "https://inventory-management-controge-8.onrender.com";
 
 function getHost(){
 	return(mode == 0) ? host_local : host_remote;
